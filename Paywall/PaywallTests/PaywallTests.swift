3b4ba7315241f1b612755b4bf810706d432e58a5
Jeremy Gerow <jeremygerow@gmail.com>
2021-02-24 22:46:23 -0800

    Added unit testing

--- Paywall/PaywallTests/PaywallTests.swift
+++ Paywall/PaywallTests/PaywallTests.swift
@@ -9,5 +9,25 @@ import XCTest
 @testable import Paywall
 
 class PaywallTests: XCTestCase {
-  
+    func testService() {
+        let expection = expectation(description: "PaywallService")
+        let paywallService = PaywallService()
+        paywallService.loadData { paywallSKU in
+            guard let paywallSKU = paywallSKU else {
+                XCTFail("PaywallSKU is missing")
+                return
+            }
+            XCTAssertEqual(paywallSKU.title, "The best stories in all the world, all in one place.")
+            XCTAssertEqual(paywallSKU.actionSignUp, "SIGN UP NOW")
+            XCTAssertEqual(paywallSKU.description, "Start streaming Disney+ with your 7 day free trial, then only 6.99/month.")
+            XCTAssertEqual(paywallSKU.sku, "dplus_free_trial")
+            XCTAssertEqual(paywallSKU.backgroundImage, "http://localhost:8000/images/splash.png")
+            XCTAssertEqual(paywallSKU.brandsImage, "http://localhost:8000/images/brands.png")
+            XCTAssertEqual(paywallSKU.logoImage, "http://localhost:8000/images/logo.png")
+            XCTAssertEqual(paywallSKU.actionLogIn, "Log In")
+            XCTAssertEqual(paywallSKU.isFullscreen, true)
+            expection.fulfill()
+        }
+        waitForExpectations(timeout: 60, handler: nil)
+    }
 }
